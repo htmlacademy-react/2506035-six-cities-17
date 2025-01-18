d663b3730c98425dcdef825af191b1a47bb78879
DanilaTkachenko <olrait9@gmail.com>
2025-01-18 21:49:55 +0200

    фикс ссылки в футере

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import { RoutePath } from '../../const';
 
 function Footer(): JSX.Element {
   return (
     <footer className="footer">
-      <Link className="footer__logo-link" to="main.html">
+      <Link className="footer__logo-link" to={RoutePath.Main}>
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33} />
       </Link>
     </footer>
